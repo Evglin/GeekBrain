4c9ff6ec81d3479d440d52edce9b62da2435285f
Evgeniy Lindenboy <evglinmail@yandex.ru>
2021-12-26 19:22:08 +1000

    lesson2_exercise1_finished
    lesson2_exercise1_finished

---
+++ Homework_lesson2/exercise1/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите температуру 1: ");
+            int Temp1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите температуру 2: ");
+            int Temp2 = Convert.ToInt32(Console.ReadLine());
+
+            double AverageTemp = Convert.ToDouble(Temp1 + Temp2) / 2;
+            Console.Write($"Средняя температура: {Convert.ToString(AverageTemp)}");
+            Console.ReadKey();
+        }
+    }
+}

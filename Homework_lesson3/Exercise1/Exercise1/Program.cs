a393c69a2c8afb635d1ba9720a09b676144caa6d
Evgeniy Lindenboy <evglinmail@yandex.ru>
2021-12-28 20:18:57 +1000

    Lesson3_ex1_finished
    Lesson3_ex1_finished

---
+++ Homework_lesson3/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Размер массива: ");
+            int ArraySize = Convert.ToInt32(Console.ReadLine());
+            int[,] Array = new int[ArraySize, ArraySize];
+
+            //Заполнение массива случайными числами (до 9)
+            for (int i= 0; i < ArraySize; i++)
+            {
+                for (int j = 0; j < ArraySize; j++)
+                {
+                    Random rnd = new Random();
+                    Array[i, j] = rnd.Next(1,9);
+                }
+            }
+
+            //вывод всего массива
+            for (int i = 0; i < ArraySize; i++)
+            {
+                for (int j = 0; j < ArraySize; j++)
+                {
+                    Console.Write($"{Array[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            //вывод диагонали
+            for (int i = 0; i < ArraySize; i++)
+            {
+                for (int j = 0; j < ArraySize; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write($"{Array[i, j]} ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

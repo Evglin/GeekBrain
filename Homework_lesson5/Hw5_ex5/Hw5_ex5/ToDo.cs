3c38f23671d2e6070933e97fd4a663bdbd537547
Evgeniy Lindenboy <evglinmail@yandex.ru>
2022-01-18 16:43:22 +1000

    Lesson5_exercise5_finished
    Lesson5_exercise5_finished

---
+++ Homework_lesson5/Hw5_ex5/Hw5_ex5/ToDo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw5_ex5
+{
+    class ToDo
+    {
+        private string _title;
+        private bool _isDone;
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set { _isDone = value; }
+        }
+
+        public string AddTask()
+        {
+            System.Console.Write("Введите текст задачи:");
+            return System.Console.ReadLine();
+        }
+
+        public void PrintTask(int i)
+        {
+            string printedDone = "";
+            switch (_isDone)
+            {
+                case true:
+                    printedDone = "[X] ";
+                    break;
+                case false:
+                    printedDone = "[O] ";
+                    break;
+            }
+            System.Console.WriteLine($"{i}. \t {printedDone + _title}");
+        }
+    }
+}

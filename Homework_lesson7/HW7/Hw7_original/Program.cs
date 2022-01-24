814d2ddb2412fccad4f46317e052f1c5938d57d8
Evgeniy Lindenboy <evglinmail@yandex.ru>
2022-01-24 14:36:52 +1000

    Hw7_finished
    Hw7_finished

---
+++ Homework_lesson7/HW7/Hw7_original/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Hw7_ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secret = "qwerty";
+            Console.WriteLine("Enter password:");
+            string input = Console.ReadLine();
+            if (input == secret)
+            {
+                Console.WriteLine("Welcome!");
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+
+        }
+    }
+}
---
+++ Homework_lesson7/HW7/Hw7_patched/Program.cs
@@ -0,0 +1,25 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Hw7_ex1.Program
+// Assembly: Hw7_ex1, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 0493D87E-C7D8-49F8-A73E-2DEB0819CC10
+// Assembly location: D:\GIT\GeekBrain\Homework_lesson7\HW7_ex1\Hw7_ex1\bin\Release\net5.0\Hw7_ex1.dll
+
+using System;
+
+namespace Hw7_ex1
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      string str = "qwerty";
+      Console.WriteLine("Enter password:");
+      if (Console.ReadLine() != str)
+        Console.WriteLine("Welcome!");
+      else
+        Console.WriteLine("Error!");
+        Console.ReadKey();
+    }
+       
+  }
+}

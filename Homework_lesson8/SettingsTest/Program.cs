86b0e1d61d5fdcbe8aaf136d7c1a777d659fe9d6
Evgeniy Lindenboy <evglinmail@yandex.ru>
2022-01-25 01:09:18 +1000

    Hw8_settings_finished
    Hw8_settings_finished

---
+++ Homework_lesson8/SettingsTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SettingsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Properties.Settings.Default.UserName == "")
+            {
+                Console.Write("Ваше имя: ");
+                Properties.Settings.Default.UserName = Console.ReadLine();
+                Console.Write("Возраст: ");
+                Properties.Settings.Default.Age = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Вид деятельности: ");
+                Properties.Settings.Default.Job = Console.ReadLine();
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                Console.WriteLine($"{Properties.Settings.Default.Greetings}, {Properties.Settings.Default.UserName}, Ваш возраст: {Properties.Settings.Default.Age}, вы работаете в {Properties.Settings.Default.Job} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
